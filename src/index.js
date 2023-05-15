c21cce4224e8c29c14cec007b89323fcc1dc5856
Vadim Yatskul <vadimajj@gmail.com>
2023-05-15 20:57:26 +0300

    deleted persist redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store';
 import App from 'App';
 import './index.css';
 import { store } from './redux/store';
@@ -12,9 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-07-phonebook">
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,4 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { contactSlice } from './contactSlice';
 import { filterSlice } from './filterSlice';
 
@@ -17,24 +6,6 @@ const rootReducer = combineReducers({
   contacts: contactSlice.reducer,
   filter: filterSlice.reducer,
 });
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
